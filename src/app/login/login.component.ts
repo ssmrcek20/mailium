e60c8d8fbd391ee485abec1298afa345f6ed100a
ssmrcek20 <100624076+ssmrcek20@users.noreply.github.com>
2024-01-05 01:01:50 +0100

    Added registration link to login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,11 +4,12 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule } from '@angular/forms'; 
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatIconModule, MatButtonModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
